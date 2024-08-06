63aa3762f40512c9b28bf903eca59e5cb83d3c93
SDFTDusername <85052868+SDFTDusername@users.noreply.github.com>
2024-08-06 13:57:18 +0200

    Made /spawn teleport if there already is a pathfinder

--- src/main/java/com/sdftdusername/pathfinding/commands/CommandSpawn.java
+++ src/main/java/com/sdftdusername/pathfinding/commands/CommandSpawn.java
@@ -13,6 +13,15 @@ public class CommandSpawn extends AdvancedCommand {
     public void run(Map<String, String> args) {
         super.run(args);
 
+        for (int i = 0; i < zone.allEntities.size; ++i) {
+            Entity entity = zone.allEntities.get(i);
+            if (entity.entityTypeId.equals(PathfinderEntity.ENTITY_TYPE_ID)) {
+                entity.setPosition(player.getPosition());
+                sendMessage("Teleported " + entity.entityTypeId);
+                return;
+            }
+        }
+
         Entity pathfinder = EntityCreator.get(PathfinderEntity.ENTITY_TYPE_ID);
         pathfinder.setPosition(player.getPosition());
         zone.addEntity(pathfinder);
