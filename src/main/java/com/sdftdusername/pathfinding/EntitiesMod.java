0d02d0f0abbf962c6ad8efe794fe79d2d314e06f
SDFTDusername <85052868+SDFTDusername@users.noreply.github.com>
2024-08-06 13:18:52 +0200

    Use the ENTITY_TYPE_ID field in EntitiesMod

--- src/main/java/com/sdftdusername/pathfinding/EntitiesMod.java
+++ src/main/java/com/sdftdusername/pathfinding/EntitiesMod.java
@@ -6,6 +6,6 @@ import finalforeach.cosmicreach.entities.EntityCreator;
 public class EntitiesMod {
     public static void RegisterEntities() {
         PathfindingMod.LOGGER.info("Registering entities");
-        EntityCreator.registerEntityCreator("base:entity_pathfinder", PathfinderEntity::new);
+        EntityCreator.registerEntityCreator(PathfinderEntity.ENTITY_TYPE_ID, PathfinderEntity::new);
     }
 }
